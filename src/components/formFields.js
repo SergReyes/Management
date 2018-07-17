f292e1e2d95ebcd55818e4dfafca5a55cd69e264
SergReyes <shsstdent01@gmail.com>
2018-07-17 10:05:00 -0600

    built sign up form

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -5,9 +5,9 @@ export class FormInput extends Component {
     render() {
         const { className, title, input, type, placeholder } = this.props;
         return (
-            <div className='form-input'>
+            <div className={`${className} form-input`}>
                 <label className="form-input__title">{title}</label>
-                <input className={`${className} form-input__input`} 
+                <input className="form-input__input"
                 type={type} 
                 {...input} 
                 placeholder={placeholder}/>
@@ -15,3 +15,21 @@ export class FormInput extends Component {
         )
     }
 }
+
+export class FormButton extends Component {
+    render() {
+        const { className, title, input, type } = this.props;
+        return (
+            <div className={`${className} form-button`}>
+                <button
+                    className="form-button__button"
+                    type={type} 
+                    {...input} 
+                >
+                    {title}
+                </button>
+            </div>
+        )
+    }
+}
+
