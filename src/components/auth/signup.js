33277fe0c07317958f93322d74eb9f9b7a0c5944
SergReyes <shsstdent01@gmail.com>
2018-07-18 08:36:09 -0600

    hoping this works

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import SignupForm from './signupForm';
+import * as actions from '../../actions';
+import { connect } from 'react-redux';
+
+
+
 
 class Signup extends Component {
+
+    onSubmit = (fields) => {
+        this.props.signUp(fields, () => {
+        console.log('navigate to dashboard');
+        this.props.history.push('/dashboard')
+    })
+}
+
+
     render() {
         return (
             <div className='sign-up'>
-                <SignupForm/>
+                <SignupForm onSubmit={(event) => this.onSubmit(event)}/>
             </div>
         )
     }
 }
 
-export default Signup;
+export default connvect(null, actions)(signUp);
